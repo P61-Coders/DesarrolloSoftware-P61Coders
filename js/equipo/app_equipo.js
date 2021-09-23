9276193669d0f51af59747bc78243d3f7570a5a8
DavidFonsecaG <davidfonsecagaona@gmail.com>
2021-09-23 00:24:18 -0500

    agregando Vue a seccion equipo

---
+++ js/equipo/app_equipo.js
@@ -0,0 +1,42 @@
+var appSeccionEquipo = new Vue({
+    el: '#app-seccion-equipo',
+    data: {
+        members: [
+            {
+                memberId: 61-1-1,
+                memberTitle: 'Product owner',
+                memberImage: 'url('+'../images/equipo/Alfonso_Gallegos.jpg'+')',
+                memberName: 'Alfonso Gallegos',
+                memberDetails: ' - Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+            },
+            {
+                memberId: 61-1-2,
+                memberTitle: 'Scrum Master',
+                memberImage: 'url('+'../images/equipo/Daniel_Gordillo.jpg'+')',
+                memberName: 'Daniel Gordillo',
+                memberDetails: ' - Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+            },
+            {
+                memberId: 61-1-3,
+                memberTitle: 'Dev Team',
+                memberImage: 'url('+'../images/equipo/David_Fonseca.jpg'+')',
+                memberName: 'David Fonseca',
+                memberDetails: ' - Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+            },
+            {
+                memberId: 61-1-4,
+                memberTitle: 'Dev Team',
+                memberImage: 'url('+'../images/equipo/David_Arias.jpg'+')',
+                memberName: 'David Arias',
+                memberDetails: ' - Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+            },
+            {
+                memberId: 61-1-5,
+                memberTitle: 'Dev Team',
+                memberImage: 'url('+'../images/equipo/miembro.jpg'+')',
+                memberName: 'Maria Paez',
+                memberDetails: ' - Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+            }
+        ]
+    }
+})
