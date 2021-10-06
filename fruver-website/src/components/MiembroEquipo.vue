0e9b933a35e22ff4ee30be7da09908f7034d2aae
DavidFonsecaG <davidfonsecagaona@gmail.com>
2021-10-05 19:25:42 -0500

    Commit before Vuetify

---
+++ fruver-website/src/components/MiembroEquipo.vue
@@ -0,0 +1,66 @@
+<template>
+    <!-- Tarjeta -->
+    <article 
+        class="col-10 col-sm-5 col-xl-2 tarjeta-miembro-equipo">
+        <div 
+            class="row miembro-equipo_img"
+            :style="{ 'backgroundImage': 'url(' + member.memberImage + ')' }">
+        </div>
+        <div class="row miembro-equipo_body">
+            <h3 class="miembro-equipo_title">{{ member.memberName }}</h3>
+            <p class="miembro-equipo_par">
+                <b>{{ member.memberTitle }}</b>
+                {{ member.memberDetails }}
+            </p>
+        </div>
+    </article>
+</template>
+
+<script>
+export default {
+    name: 'MiembroEquipo',
+    props: {
+        member: Object,
+        id: Number
+    },
+    data(){
+        return{
+            text : '',
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style scoped>
+.miembro-equipo_img {
+    background-size: cover;
+    background-position: top;
+    background-repeat: no-repeat;
+    border-top-left-radius: 15px;
+    border-top-right-radius: 15px;
+    margin-bottom: 30px;
+    height: 320px;
+}
+
+.miembro-equipo_title{
+    font-family: 'Fredoka One', cursive;
+    text-align: center;
+    font-size: 18px;
+}
+
+.miembro-equipo_par{
+    font-family: 'Roboto', sans-serif;
+    text-align: center;
+}
+
+@media (min-width: 1200px) {
+
+    .miembro-equipo_img {
+        background-position: top;
+        height: 270px;
+    }
+}
+</style>
---
+++ fruver-website/src/views/Home.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+
+    <!-- Seccion Equipo - Desarrollada por David Fonseca -->
+    <section class="seccion-equipo">
+        <!-- Titulo de Seccion - Equipo -->
+        <div>
+            <h2 class="seccion-equipo_title">{{ title }}</h2>
+        </div>
+        <!-- Seccion de tarjetas - Equipo --> 
+        <div class="row justify-content-center seccion-equipo_body">
+            <miembro-equipo
+                v-for="(member, id) in members" :key="member.memberId"
+                :member="member"
+                :index="id"
+            ></miembro-equipo>
+        </div>
+    </section>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+import MiembroEquipo from '@/components/MiembroEquipo.vue';
+import miembrosData from '@/assets/resources/data.json';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    MiembroEquipo
+  },
+  data() {
+    return {
+        projectName: 'Nuestro Equipo',
+        group: 'Grupo 61',
+        members: miembrosData.members,
+    };
+  },
+  methods: {
+    setMembers: function () {
+        console.log(this.members);
+    }
+  },
+  computed: {
+    title() {
+      return this.projectName + " - " + this.group;
+    }
+  },
+  created() {
+    if(this.members){
+        this.setMembers();
+    }
+  },
+};
+</script>
+
+<style scoped>
+/* selectores */
+
+/* // X-Small devices (portrait phones, less than 576px)
+// No media query for `xs` since this is the default in Bootstrap */
+
+.seccion-equipo {
+    margin-top: 100px;
+    margin-bottom: 140px;
+}
+
+.seccion-equipo_title{
+    margin-bottom: 80px;
+    text-align: center;
+    font-size: 50px;
+    font-family: 'Fredoka One', cursive;
+}
+
+.tarjeta-miembro-equipo {
+    -webkit-transition: -webkit-transform 1s linear;
+    -moz-transition: -moz-transform 1s linear;
+    -o-transition: -o-transform 1s linear;
+    -ms-transition: -ms-transform 1s linear;
+    transition: transform 0.5s linear;
+}
+
+.tarjeta-miembro-equipo:hover {
+    -webkit-transform: scale(2, 2);
+    -moz-transform: translate(2px, 1px);
+    -o-transform: translate(2px, 1px);
+    -ms-transform: translate(2px, 1px);
+    transform: scale(1.05, 1.05);
+}
+
+.tarjeta-miembro-equipo {
+    margin: 10px;
+    margin-bottom: 20px;
+    height: 450px;
+    box-shadow: 0px 0px 20px 5px rgba(0, 0, 0, 0.15);
+    border-radius: 15px;
+    background-color: rgb(255, 255, 100);
+}
+
+
+/* // Small devices (landscape phones, 576px and up) */
+@media (min-width: 751px) {
+
+}
+
+/* // Medium devices (tablets, 768px and up) */
+@media (min-width: 768px) { 
+    
+}
+
+/* // Large devices (desktops, 992px and up) */
+@media (min-width: 992px) {
+    
+}
+
+/* // X-Large devices (large desktops, 1200px and up) */
+@media (min-width: 1200px) {
+    .tarjeta-miembro-equipo {
+        height: 430px;
+    }
+
+}
+
+/* // XX-Large devices (larger desktops, 1400px and up) */
+@media (min-width: 1400px) {
+    .tarjeta-miembro-equipo {
+        height: 410px;
+    }
+}
+</style> 
