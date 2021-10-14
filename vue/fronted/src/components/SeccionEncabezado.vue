b8926c5926722faaeb6c3fe37c06b1ecc8ca0a19
DavidFonsecaG <davidfonsecagaona@gmail.com>
2021-10-14 15:48:17 -0500

    Actualizacion Header

--- vue/fronted/src/components/SeccionEncabezado.vue
+++ vue/fronted/src/components/SeccionEncabezado.vue
@@ -1,60 +1,55 @@
 <template >
   <v-app-bar color="white" flat dense prominent>
-    <div>
-      <a href="http://localhost:8080/">
-        <img class="nav_logo" src="../assets/fruver-logo.png" alt="Fruver">
-        <n-app-title>Fruver</n-app-title>
-      </a>
-    </div>
+    <v-container>
+      <v-row align="center" justify="center">
+        <v-col cols="2">
+          <a href="http://localhost:8080/">
+            <img
+              class="nav_logo"
+              src="../assets/fruver-logo.png"
+              alt="Fruver"
+            />
+            <v-app-bar-title>Fruver</v-app-bar-title>
+          </a>
+        </v-col>
 
-    <v-spacer></v-spacer>
+        <v-col id="nav" cols="8">
+          <router-link :to="{ name: 'Home' }">Home</router-link> |
 
-    <div id="nav">
-      <router-link :to="{ name: 'Home' }">Home</router-link> |
+          <v-menu>
+            <template v-slot:activator="{ on: menu, attrs }">
+              <v-tooltip bottom>
+                <template v-slot:activator="{ on: tooltip }">
+                  <v-btn plain v-bind="attrs" v-on="{ ...tooltip, ...menu }">
+                    Tienda
+                  </v-btn>
+                </template>
+                <span>Im A ToolTip</span>
+              </v-tooltip>
+            </template>
+            <v-list>
+              <v-list-item v-for="(item, index) in items" :key="index">
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
 
-      <div class="text-center">
-        <v-menu>
-          <template v-slot:activator="{ on: menu, attrs }">
-            <v-tooltip bottom>
-              <template v-slot:activator="{ on: tooltip }">
-                <v-btn
-                  plain
-                  v-bind="attrs"
-                  v-on="{ ...tooltip, ...menu }"
-                >
-                  Tienda
-                </v-btn>
-              </template>
-              <span>Im A ToolTip</span>
-            </v-tooltip>
-          </template>
-          <v-list>
-            <v-list-item
-              v-for="(item, index) in items"
-              :key="index"
-            >
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-      </div>
+          <router-link :to="{ name: 'Tienda' }">Tienda</router-link> |
 
-      <router-link :to="{ name: 'Tienda' }">Tienda</router-link> |
+          <router-link :to="{ name: 'Nosotros' }">Nosotros</router-link>
+        </v-col>
 
-      <router-link :to="{ name: 'Nosotros' }">Nosotros</router-link>
-    </div>
+        <v-col cols="2">
+          <v-btn icon :to="{ name: 'Login' }">
+            <v-icon>mdi-account</v-icon>
+          </v-btn>
 
-    <v-spacer></v-spacer>
-
-    <div>
-      <v-btn icon :to="{ name: 'Login' }">
-          <v-icon>mdi-account</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-cart</v-icon>
-      </v-btn>
-    </div>
+          <v-btn icon>
+            <v-icon>mdi-cart</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app-bar>
 </template>
 
@@ -62,13 +57,13 @@
 export default {
   name: "Encabezado",
   data: () => ({
-      items: [
-        { title: 'Click Me1' },
-        { title: 'Click Me2' },
-        { title: 'Click Me3' },
-        { title: 'Click Me4' },
-      ],
-    }),
+    items: [
+      { title: "Click Me1" },
+      { title: "Click Me2" },
+      { title: "Click Me3" },
+      { title: "Click Me4" },
+    ],
+  }),
 };
 </script>
 
