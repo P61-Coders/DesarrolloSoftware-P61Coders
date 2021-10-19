9604ddf477f035bac0e4b3234f0798e9c0e02763
DavidFonsecaG <davidfonsecagaona@gmail.com>
2021-10-18 21:59:02 -0500

    CSS footer

---
+++ vue/fronted/src/components/seccionFooter/FormularioSuscribirse.vue
@@ -0,0 +1,40 @@
+<template>
+     <form action="" class="row">
+        <input
+        class="col-8 input-form"
+        type="email"
+        name="EMAIL"
+        placeholder=" Ingresa tu correo "
+        />
+        <button-2 class="col-3" type="submit" :name="buttonName">
+            Suscribirse
+        </button-2>
+    </form>
+</template>
+
+<script>
+import Button2 from '../Button2.vue';
+
+export default {
+    components: {
+        Button2,
+    },
+    data() {
+        return{
+            buttonName: "Enviar",
+        }
+    },
+}
+</script>
+
+<style scoped>
+.input-form {
+    border: 1px solid #b5b9be;
+    border-radius: 25px;
+    margin-right: 15px;
+    outline: none;
+    color: #707b8e;
+    font-family: "Poppins", sans-serif;
+    font-size: 14px;
+}
+</style>
--- vue/fronted/src/components/seccionFooter/SeccionFooter.vue
+++ vue/fronted/src/components/seccionFooter/SeccionFooter.vue
@@ -1,129 +1,78 @@
 <template>
     <!-- Seccion Footer - Desarrollada por David Fonseca -->
     <section class="seccion-footer">
-     <!-- Footer Start-->
-      <div class="footer-main">
-        <div class="footer-area footer-padding">
-            <div class="container">
-                <div class="row  justify-content-between">
-                    <div class="col-lg-3 col-md-4 col-sm-8">
-                         <div class="single-footer-caption mb-30">
-                              <!-- logo -->
-                             <div>
-                                <router-link class="footer-logo" :to="{name: 'Home'}">
-                                    Fruver
-                                </router-link>
-                             </div>
-                             <div class="footer-tittle">
-                                 <div class="footer-links">
-                                     <p class="info1">https://github.com/P61-Coders/DesarrolloSoftware-P61Coders.git</p>
-                                </div>
-                             </div>
-                         </div>
-                    </div>
-                    <div class="col-lg-2 col-md-4 col-sm-5">
-                        <div class="single-footer-caption mb-50">
-                            <div class="footer-tittle">
-                                <h4>Quick Links</h4>
-                                <ul class="footer-links">
-                                    <li><a href="#">Nosotros</a></li>
-                                    <li><a href="#">Calendario</a></li>
-                                    <li><a href="#">Contactanos</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-4 col-sm-7">
-                        <div class="single-footer-caption mb-50">
-                            <div class="footer-tittle">
-                                <h4>Support</h4>
-                                <ul class="footer-links">
-                                    <li><a href="#">Frutas</a></li>
-                                    <li><a href="#">Verduras</a></li>
-                                    <li><a href="#">Exoticas</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-8">
-                        <div class="single-footer-caption mb-50">
-                            <div class="footer-tittle">
-                                <h4>Novedades</h4>
-                                <div class="footer-links">
-                                 <p>Quieres recibir novedades. Suscribete!</p>
-                             </div>
-                             <!-- Form -->
-                             <div class="footer-form">
-                                 <div id="mc_embed_signup">
-                                     <form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="subscribe_form relative mail_part" novalidate="true">
-                                         <input type="email" name="EMAIL" id="newsletter-form-email" placeholder=" Ingresa tu correo " class="placeholder hide-on-focus" onfocus="this.placeholder = ''" onblur="this.placeholder = ' Email Address '">
-                                         <div class="form-icon">
-                                             <button type="submit" name="submit" id="newsletter-submit" class="email_icon newsletter-submit button-contactForm"><img src="assets/img/shape/form_icon.png" alt=""></button>
-                                         </div>
-                                         <div class="mt-10 info"></div>
-                                     </form>
-                                 </div>
-                             </div>
-                            </div>
-                        </div>
-                    </div>
+        <!-- Footer Start-->
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-lg-4 col-md-4 col-sm-8">
+                    <!-- logo -->
+                    <router-link class="row footer-logo" :to="{ name: 'Home' }">
+                        Fruver
+                    </router-link>
                 </div>
-                <!-- Copy-Right -->
-                <div class="row align-items-center">
-                    <div class="col-xl-12 ">
-                        <div class="footer-copy-right">
-                          
-                        </div>
-                    </div>
+                <!-- Form -->
+                <div class="col-lg-8 col-md-4 col-sm-8 form-footer">
+                    <formulario-suscribirse/>
                 </div>
             </div>
+            <hr>
+
+                            
+            <div class="row">
+                <a 
+                    class="col-8 row footer-repo" 
+                    href="https://github.com/P61-Coders/DesarrolloSoftware-P61Coders.git"
+                    target="_blank"
+                >Repo Github https://github.com/P61-Coders/DesarrolloSoftware-P61Coders.git</a>
+                <h6 class="col-4 text-end">Desarrollado por David Fonseca</h6>
+            </div>
         </div>
-      </div>
-       <!-- Footer End-->
-        
     </section>
 </template>
 
 <script>
-export default {};
+import FormularioSuscribirse from './FormularioSuscribirse.vue';
+
+export default {
+    components: {
+        FormularioSuscribirse,
+    },
+    data() {
+        return {
+
+        };
+    },
+};
 </script>
 
 <style scoped>
 .seccion-footer {
-  margin-top: 100px;
   padding-top: 150px;
-  padding-bottom: 150px;
+  padding-bottom: 80px;
 }
 
-a {
+.footer-repo {
   text-decoration: none;
   color: #707b8e;
+  font-family: "Poppins", sans-serif;
+  padding-left: 35px;
 }
 
 .footer-logo {
+    text-decoration: none;
     color: #2b044d;
-    font-family: 'Nunito', sans-serif;
+    font-family: "Nunito", sans-serif;
     font-size: 40px;
-    padding-bottom: 50px;
+    padding-left: 35px;
 }
 
-.footer-tittle{
-    font-family: 'Nunito', sans-serif;
-    font-size: 15px;
-
+.form-footer {
+    align-content: center;
 }
 
-.footer-links {
-    font-family: 'Poppins', sans-serif;
-    font-size: 16px;
-    line-height: 2;
+.text-end {
     color: #707b8e;
-}
-
-li {
-    color: #707b8e;
-    max-width: 410px;
-    line-height: 2.5;
+    font-family: "Poppins", sans-serif;
 }
 
 </style>
