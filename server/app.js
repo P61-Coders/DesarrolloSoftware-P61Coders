d91a483f15d9225103cd8bf8b60b9c85085e2a35
DavidFonsecaG <davidfonsecagaona@gmail.com>
2021-10-18 23:16:48 -0500

    after heroku

--- server/app.js
+++ server/app.js
@@ -18,6 +18,16 @@ mongoose.connect(url,options).then(
 //middleware
 app.use(morgan('tiny'));
 app.use(cors());
+
+app.use((req, res, next) =>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
